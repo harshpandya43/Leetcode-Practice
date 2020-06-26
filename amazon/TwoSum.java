23232a3e659bccda58eaea9439900554333155ae
Harsh Pandya <pandyaharsh43@gmail.com>
2020-06-25 17:39:43 -0700

    Day 1

---
+++ amazon/TwoSum.java
@@ -0,0 +1,45 @@
+package amazon;
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+
+		TwoSum t = new TwoSum();
+		int[] sol = t.twoSumSol(nums, target);
+
+		for (int s : sol) {
+			System.out.println(s);
+		}
+
+	}
+
+	public int[] twoSumSol(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = nums[i] - target;
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[] { i, map.get(complement) };
+			}
+		}
+		throw new IllegalArgumentException("Exception");
+	}
+
+}
